b2fc6fce1d39b537ef34fdc195931521435018a9
imshn <shaan@vyomr.in>
2024-06-27 10:08:46 +0530

    Demo ready!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import { useEffect } from "react";
 
-function App() {
+import "./App.css";
+
+const DOMAIN_ID = process.env.REACT_APP_DOMAIN_ID;
+const ENV_ID = process.env.REACT_APP_ENV_ID;
+console.log(import.meta.env);
+const imagesUrls = [
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/car/" +
+    ENV_ID +
+    "/android",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/air001/" +
+    ENV_ID +
+    "/web",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/flower001/" +
+    ENV_ID +
+    "/generic",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/house001/" +
+    ENV_ID +
+    "/ios",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/mobile001/" +
+    ENV_ID +
+    "/android",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/earth001/" +
+    ENV_ID +
+    "/web",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/animal001/" +
+    ENV_ID +
+    "/android",
+  "https://adn.nestortech.io/api/va/" +
+    DOMAIN_ID +
+    "/ship001/" +
+    ENV_ID +
+    "/web"
+];
+export default function App() {
+  const renderImage = () => {
+    return imagesUrls?.map((url, index) => (
+      <div key={index}>
+        <img src={url} />
+      </div>
+    ));
+  };
+
+  useEffect(() => {
+    renderImage();
+  }, [imagesUrls]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{ textAlign: "center", margin:"20px 0px" }}>Nestor Assets Demo - {ENV_ID}</h1>
+      <div className="gallery">{renderImage()}</div>
     </div>
   );
 }
-
-export default App;
